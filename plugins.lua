d4514d5cde9981273c32f6a562d15e2f1c2ab8b4
stickburn <yashank@hey.com>
2024-02-07 00:02:01 +0530

    feat: plugins to mod

--- plugins.lua
+++ plugins.lua
@@ -3,8 +3,45 @@ local overrides = require "custom.configs.overrides"
 ---@type NvPluginSpec[]
 local plugins = {
   {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    config = function()
+      require "custom.configs.conform"
+    end,
+  },
+
+  {
     "nvim-telescope/telescope.nvim",
     opts = overrides.telescope,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "BufReadPost",
+    opts = overrides.context,
   },
 }
 
