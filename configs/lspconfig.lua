920beca3374bfdb6b98bb1478704cc82c801b970
stickburn <yashank@hey.com>
2024-02-10 02:39:59 +0530

    feat: setup lsp

---
+++ configs/lspconfig.lua
@@ -0,0 +1,18 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+local servers = { "html", "cssls", "tsserver", "rust_analyzer", "lua_ls" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+vim.diagnostic.config {
+  signs = false,
+  underline = false,
+}
